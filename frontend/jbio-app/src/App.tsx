71028821d34062253461ccee7d88da295667a6cd
Zach Van Horn <zvanhorn10@gmail.com>
2025-09-18 12:24:41 -0400

    Updated icons for 1DE and 2DE.

--- frontend/jbio-app/src/App.tsx
+++ frontend/jbio-app/src/App.tsx
@@ -7,15 +7,16 @@ import OneDE from './pages/legacy/1DElectrophoresis.js'
 
 import GitHubIcon from '@mui/icons-material/GitHub';
 import HomeIcon from '@mui/icons-material/HomeRounded';
-import ShowChartIcon from '@mui/icons-material/ShowChart';
-import BubbleChartIcon from '@mui/icons-material/BubbleChart';
+import { ReactComponent as OneDEIcon } from './assets/1DE.svg';
+import { ReactComponent as TwoDEIcon } from './assets/2DE.svg';
+
 
 
 function App() {
   const pages = [
     { id: 1, icon: <HomeIcon />, component: <About />, label: 'Project Information' },
-    { id: 2, icon: <ShowChartIcon />, component: <OneDE />, label: '1D Electrophoresis' },
-    { id: 3, icon: <BubbleChartIcon />, component: <Placeholder text='WIP Team' />, label: '2D Electrophoresis' },
+    { id: 2, icon: <OneDEIcon />, component: <OneDE />, label: '1D Electrophoresis' },
+    { id: 3, icon: <TwoDEIcon />, component: <Placeholder text='WIP Team' />, label: '2D Electrophoresis' },
     { id: 4, icon: <GitHubIcon />, link: 'https://github.com/paulcraig/ProSep', label: 'GitHub Repository' },
   ];
 
