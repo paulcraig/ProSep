aa121d3e8efeb12c5b92059fae3dc63c77f6cd72
jacob-fay <71296174+jacob-fay@users.noreply.github.com>
2025-10-04 00:05:04 -0400

    fixed 1d api endpoint

--- backend/api/one_de_routes.py
+++ backend/api/one_de_routes.py
@@ -6,7 +6,7 @@ router = APIRouter(
     prefix="/1d",
     tags=["1D Simulation"]
 )
-@router.post('/protein_info', response_model=list[Any])
+@router.post('/ProteinInfo/File', response_model=list[Any])
 async def fileGetProteinInfo(file: UploadFile) -> Any:
     
     return Simulation_1de.fileGetProteinInfo(file)
@@ -15,6 +15,6 @@ async def fileGetProteinInfo(file: UploadFile) -> Any:
 
 
 
-@router.post('/batch_files', response_model=list[list[Any]])
+@router.post('/BatchFileProtein/Batch', response_model=list[list[Any]])
 async def batchFileGetProteinInfo(files: list[UploadFile]) -> Any:
     return Simulation_1de.batchFileGetProteinInfo(files)
