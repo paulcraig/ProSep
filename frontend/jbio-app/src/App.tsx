50771570674728820208c589fc8936ebcbbd05ef
Zach Van Horn <zvanhorn10@gmail.com>
2025-10-16 16:30:31 -0400

    Temp fix for icon + shortening page name...

--- frontend/jbio-app/src/App.tsx
+++ frontend/jbio-app/src/App.tsx
@@ -7,6 +7,8 @@ import TwoDE from './pages/legacy/2DElectrophoresis.js'
 
 import GitHubIcon from '@mui/icons-material/GitHub';
 import HomeIcon from '@mui/icons-material/HomeRounded';
+import InsightsIcon from "@mui/icons-material/Insights";
+
 import { ReactComponent as OneDEIcon } from './assets/electrophoresis/1DE.svg';
 import { ReactComponent as TwoDEIcon } from './assets/electrophoresis/2DE.svg';
 import PeptideRetention from './pages/PeptideRetention';
@@ -16,7 +18,7 @@ function App() {
     { id: 1, icon: <HomeIcon />, component: <About />, label: 'Project Information' },
     { id: 2, icon: <OneDEIcon />, component: <OneDE />, label: '1D Electrophoresis' },
     { id: 3, icon: <TwoDEIcon />, component: <TwoDE />, label: '2D Electrophoresis' },
-    { id: 4, icon: <HomeIcon/>, component: <PeptideRetention />, label: 'Peptide Retention Prediction' },
+    { id: 4, icon: <InsightsIcon/>, component: <PeptideRetention />, label: 'Peptide Retention' },
     { id: 4, icon: <GitHubIcon />, link: 'https://github.com/paulcraig/ProSep', label: 'GitHub Repository' },
   ];
 
