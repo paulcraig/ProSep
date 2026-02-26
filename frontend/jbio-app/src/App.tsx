4004e035228dd0c00fcdc7dbda13f6ca58c61e96
Aditya Vikram <adityavikram2016@gmail.com>
2026-02-26 15:40:51 -0500

    app page update to add new page

--- frontend/jbio-app/src/App.tsx
+++ frontend/jbio-app/src/App.tsx
@@ -1,37 +1,70 @@
-import logo from './assets/basil-logo.png';
-import Dashboard from './components/Dashboard';
-import About from './pages/About';
+import logo from "./assets/basil-logo.png";
+import Dashboard from "./components/Dashboard";
+import About from "./pages/About";
 
-import OneDE from './pages/1DE'
-import TwoDE from './pages/legacy/2DElectrophoresis.js'
+import OneDE from "./pages/1DE";
+import TwoDE from "./pages/legacy/2DElectrophoresis.js";
 
-import GitHubIcon from '@mui/icons-material/GitHub';
-import HomeIcon from '@mui/icons-material/HomeRounded';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import HomeIcon from "@mui/icons-material/HomeRounded";
 import InsightsIcon from "@mui/icons-material/Insights";
+import SwapVertIcon from '@mui/icons-material/SwapVert';
+import AlignHorizontalCenterIcon from '@mui/icons-material/AlignHorizontalCenter';
 
-import { ReactComponent as OneDEIcon } from './assets/electrophoresis/1DE.svg';
-import { ReactComponent as TwoDEIcon } from './assets/electrophoresis/2DE.svg';
-import PeptideRetention from './pages/PeptideRetention';
-import ProteolyticDigestion from './pages/ProteolyticDigestion';
+import { ReactComponent as OneDEIcon } from "./assets/electrophoresis/1DE.svg";
+import { ReactComponent as TwoDEIcon } from "./assets/electrophoresis/2DE.svg";
+
+import PeptideRetention from "./pages/PeptideRetention";
+import ProteolyticDigestion from "./pages/ProteolyticDigestion";
+import IonExchangeFractionation from "./pages/IonExchangeFractionation";
 
 function App() {
   const pages = [
-    { id: 1, icon: <HomeIcon />, component: <About />, label: 'Project Information' },
-    { id: 2, icon: <OneDEIcon />, component: <OneDE />, label: '1D Electrophoresis' },
-    { id: 3, icon: <TwoDEIcon />, component: <TwoDE />, label: '2D Electrophoresis' },
-    { id: 4, icon: <InsightsIcon/>, component: <PeptideRetention />, label: 'Peptide Retention' },
-    { id: 5, icon: <InsightsIcon/>, component: <ProteolyticDigestion />, label: 'Proteolytic Digestion' },
-    { id: 6, icon: <GitHubIcon />, link: 'https://github.com/paulcraig/ProSep', label: 'GitHub Repository' },
+    {
+      id: 1,
+      icon: <HomeIcon />,
+      component: <About />,
+      label: "Project Information",
+    },
+    {
+      id: 2,
+      icon: <OneDEIcon />,
+      component: <OneDE />,
+      label: "1D Electrophoresis",
+    },
+    {
+      id: 3,
+      icon: <TwoDEIcon />,
+      component: <TwoDE />,
+      label: "2D Electrophoresis",
+    },
+    {
+      id: 4,
+      icon: <InsightsIcon />,
+      component: <PeptideRetention />,
+      label: "Peptide Retention",
+    },
+    {
+      id: 5,
+      icon: <AlignHorizontalCenterIcon />,
+      component: <ProteolyticDigestion />,
+      label: "Proteolytic Digestion",
+    },
+    {
+      id: 6,
+      icon: <SwapVertIcon />,
+      component: <IonExchangeFractionation />,
+      label: "Ion Exchange Fractionation",
+    },
+    {
+      id: 7,
+      icon: <GitHubIcon />,
+      link: "https://github.com/paulcraig/ProSep",
+      label: "GitHub Repository",
+    },
   ];
 
-  return (
-    <Dashboard
-      pages={pages}
-      homepage={1}
-      darkmode={true}
-      logo={logo}
-    />
-  );
+  return <Dashboard pages={pages} homepage={1} darkmode={true} logo={logo} />;
 }
 
 export default App;
