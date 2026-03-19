0a7cb015058769c3e12912ffc24e6eaee61f8cb6
jacob-fay <71296174+jacob-fay@users.noreply.github.com>
2026-03-19 12:31:07 -0400

    fixed acceidently channge of server

--- backend/server.py
+++ backend/server.py
@@ -7,7 +7,7 @@ import backend.api.two_de_routes as two_de_routes
 import backend.api.peptide_retention_routes as peptide_retention_routes
 import backend.api.proteolytic_digestion_routes as proteolytic_digestion_routes
 import backend.api.ion_exchange_fractionation_routes as ion_exchange_fractionation_routes
-import backend.api.size_exclusion as size_exclustion
+import backend.api.size_exclusion as size_exclustion_routes
 
 """
 HOW TO START UP API SERVER:
@@ -56,7 +56,8 @@ app.include_router(one_de_routes.router)
 app.include_router(two_de_routes.router)
 app.include_router(peptide_retention_routes.router)
 app.include_router(proteolytic_digestion_routes.router)
-
+app.include_router(ion_exchange_fractionation_routes.router)
+app.include_router(ion_exchange_fractionation_routes.router)
 """ 
 NOTE FOR FUTURE DEVELOPERS:
 In order to add additional files for the API, such as the addition of 2DE,
